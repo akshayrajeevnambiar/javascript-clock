b2d9ce81d0bd449ac06acea9c2e0e8a710a4d1a8
Akshay-Rajeev-Nambiar <akshayrajeevnambiar@gmail.com>
2024-01-29 02:03:20 -0500

    time function added

--- javascript-clock/src/clock.tsx
+++ javascript-clock/src/clock.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
 function Clock() {
 
-    const currentTime = new Date();
-    const hours = currentTime.getHours().toString().padStart(2, '0');
-    const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-    const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+    let time = new Date();
+    let hours = time.getHours().toString().padStart(2, '0');
+    let minutes = time.getMinutes().toString().padStart(2, '0');
+    let seconds = time.getSeconds().toString().padStart(2, '0');
+    let timeOfDay = (time.getHours() >= 12) ? "PM" : "AM";
+    
+    const [currentHours, setCurrentHours] = useState(hours);
+    const [currentMinutes, setCurrentMinutes] = useState(minutes);
+    const [currentSeconds, setCurrentSeconds] = useState(seconds);
+    const [currentTimeOfDay, setCurrentTimeOfDay] = useState(timeOfDay);
 
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const updateTime = () => {
+        let time = new Date();
+        let hours = time.getHours().toString().padStart(2, '0');
+        let minutes = time.getMinutes().toString().padStart(2, '0');
+        let seconds = time.getSeconds().toString().padStart(2, '0');
+        let timeOfDay = (time.getHours() >= 12) ? "PM" : "AM";
 
-    return <h1>${timeString}</h1>
+        setCurrentHours(hours);
+        setCurrentMinutes(minutes);
+        setCurrentSeconds(seconds);
+        setCurrentTimeOfDay(timeOfDay);
+    }
+
+    setInterval(updateTime, 1000);
+
+    return (
+        <div>
+            {currentHours}:{currentMinutes}:{currentSeconds}:{currentTimeOfDay}
+        </div>
+    );
 }
 
 export default Clock;
